4ea0bf4d5f3ccaded4455748c84305a98f88ceb2
saadp-6-6 <saadpathan800@gmail.com>
2024-11-12 09:38:00 +0530

    Update 3_numbers_exercise.py

--- Basics/Exercise/3_numbers/3_numbers_exercise.py
+++ Basics/Exercise/3_numbers/3_numbers_exercise.py
@@ -9,9 +9,7 @@ print("area of football field:",area) # Ans: 4489.599999999999
 # 2. You bought 9 packets of potato chips from a store. Each packet costs 1.49 dollar
 #    and you gave shopkeeper 20 dollar.
 #    Find out using python, how many dollars is the shopkeeper going to give you back?
-num_packets=9
-cost_per_packet=1.49
-total_cost=num_packets*cost_per_packet
+total_cost=9*1,49
 money_paid=20
 cash_back=money_paid-total_cost
 print("Cash back:",cash_back) # Ans: 6.59
